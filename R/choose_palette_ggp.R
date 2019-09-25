675ec49ebef8e122fc885f93a7560d157b2f5d4c
Felipe Zapata <fzapata@ucla.edu>
2019-09-25 15:31:07 -0700

    Updates function

--- R/choose_palette_ggp.R
+++ R/choose_palette_ggp.R
@@ -24,7 +24,7 @@ tanagr_pal <- function(palette_name) {
 #'
 #' @rdname scale_color_tanagr
 #'
-#' @param palette the name of the palette
+#' @param palette_name the name of the palette
 #'
 #' @param discrete whether to use a discrete or continuous color palette (boolean)
 #'
@@ -32,23 +32,34 @@ tanagr_pal <- function(palette_name) {
 #'
 #' @examples
 #' library(ggplot2)
-#' ggplot(iris, aes(x = Sepal.Length, y = Petal.Length, color = Species)) + geom_point(alpha = 0.5, size  = 3) + geom_density2d() + theme_bw(base_size=12) + scale_color_tanagr(palette_name = "stilpnia_preciosa")
+#' ggplot(iris, aes(x = Sepal.Length, y = Petal.Length, color = Species)) +
+#'  geom_point(size  = 3) +
+#'  geom_density2d() +
+#'  theme_bw(base_size=12) +
+#'  scale_color_tanagr(palette_name = "stilpnia_preciosa")
 #'
-#' ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, color = Petal.Width)) + geom_point(alpha = 0.6, size  = 4) +  theme_bw(base_size=12) + scale_color_tanagr(palette_name = "chlorochrysa_nitidissima", discrete = FALSE)
+#' ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, color = Petal.Width)) +
+#'  geom_point(size  = 4) +
+#'  theme_bw(base_size=12) +
+#'  scale_color_tanagr(palette_name = "chlorochrysa_nitidissima", discrete = FALSE)
 #'
 #' @export
 #'
-#' @importFrom ggplot2 discrete_scale scale_color_gradientn
+#' @importFrom ggplot2 scale_color_manual scale_color_gradientn
 
 scale_color_tanagr <- function(palette_name, discrete = TRUE, ...) {
+  palette_t = tanagr_palettes[[palette_name]]
+  n = length(palette_t)
+  pal = tanagr_pal(palette_name)
+  cols = pal(n)
+
   if (discrete) {
-    discrete_scale(aesthetics = "color",
-                   scale_name = paste0("tanagr_", palette_name),
-                   palette = tanagr_pal(palette_name),
-                   ...)
+    scale_color_manual(...,
+                        aesthetics = "color",
+                        values = cols)
   } else {
     scale_color_gradientn(...,
-                          colors = tanagr_pal(palette_name)(256),
+                          colors = pal(256),
                           aesthetics = "color")
   }
 }
@@ -57,7 +68,7 @@ scale_color_tanagr <- function(palette_name, discrete = TRUE, ...) {
 #'
 #' It generates a palette by linear interpolation for scale_fill
 #'
-#' @param palette the name of the palette
+#' @param palette_name the name of the palette
 #'
 #' @param discrete whether to use a discrete or continuous color palette (boolean)
 #'
@@ -65,21 +76,40 @@ scale_color_tanagr <- function(palette_name, discrete = TRUE, ...) {
 #'
 #' @examples
 #' library(ggplot2)
-#' ggplot(mpg, aes(manufacturer, fill = manufacturer)) + geom_bar() + theme(axis.text.x = element_text(angle = 45, hjust = 1), panel.background = element_blank()) + scale_fill_tanagr(palette = "stilpnia_preciosa", guide = "none")
+#' ggplot(mpg, aes(fl, fill = fl)) +
+#'  geom_bar() +
+#'  theme(axis.text.x = element_text(hjust = 1), panel.background = element_blank()) +
+#'  scale_fill_tanagr(palette = "stilpnia_preciosa", guide = "none")
 #'
+#' library(gridExtra)
+#' g = ggplot(faithfuld, aes(waiting, eruptions)) +
+#'  geom_raster(aes(fill = density)) +
+#'   theme_bw() +
+#'   theme(panel.grid=element_blank())
+#'
+#' grid.arrange(
+#'    g + scale_fill_tanagr(palette = "cyanerpes_cyaneus", guide = "none", discrete  = FALSE),
+#'    g + scale_fill_tanagr(palette = "dacnis_berlepschi", guide = "none", discrete  = FALSE),
+#'    g + scale_fill_tanagr(palette = "ramphocelus_sanguinolentus", guide = "none", discrete  = FALSE),
+#'    g + scale_fill_tanagr(palette = "tangara_seledon", guide = "none", discrete  = FALSE),
+#'    ncol = 2, nrow = 2
+#'    )
 #' @export
 #'
-#' @importFrom ggplot2 discrete_scale scale_fill_gradientn
+#' @importFrom ggplot2 scale_discrete_manual scale_fill_gradientn
 
 scale_fill_tanagr <- function(palette_name, discrete = TRUE, ...) {
+  palette_t = tanagr_palettes[[palette_name]]
+  n = length(palette_t)
+  pal = tanagr_pal(palette_name)
+  cols = pal(n)
+
   if (discrete) {
-    discrete_scale(aesthetics = "fill",
-                   scale_name = paste0("tanagr_", palette_name),
-                   palette = tanagr_pal(palette_name),
-                   ...)
+    scale_discrete_manual(aesthetics = "fill",
+                          values = cols,
+                          ...)
   } else {
     scale_fill_gradientn(...,
-                          colors = tanagr_pal(palette_name)(256),
-                          aesthetics = "color")
+                         colors = pal(256))
   }
 }
